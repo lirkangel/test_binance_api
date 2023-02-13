8cef3ed1fd77e5c3cd72ef0d474f89d54ed81801
Mike <lirkangel@mail.ru>
2023-02-13 15:51:28 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_binance_api
+
+For run project pipenv install and pipenv run app
---
+++ main.py
@@ -0,0 +1,27 @@
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from decouple import config
+import aiohttp
+import asyncio
+import time
+
+print('for pair XRP/USDT')
+async def main():
+    n=0
+    new_price = 0
+    async with aiohttp.ClientSession() as session:
+        while n > -1:
+            binance_url = f"{config('API_BINANCE')}{config('PAIR')}"
+            try:
+                async with session.get(binance_url) as resp:
+                    result = await resp.json()
+                    current_price = result['price']
+                    if new_price != current_price:
+                        new_price = current_price
+                        print(new_price)
+                    time.sleep(1)
+            except Exception as e:
+                print(e)
+                break
+                        
+
+asyncio.run(main())
